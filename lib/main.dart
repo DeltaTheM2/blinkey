e3d30a6dc3999a01706e597c4a7f13e875be203b
Soroush Mirzaee <72804931+DeltaTheM2@users.noreply.github.com>
2025-04-03 18:09:12 -0700

    working example page

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:blink/exampleScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: M7ExpampleScreen(),
+    );
+  }
+}
